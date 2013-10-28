bf05890e5872f4d96eeec2e4e6197600feda97e4
yanghua <yh1314529@qq.com>
2013-10-29 00:25:48 +0800

    Update fruit_diy.py

---
+++ fruit_diy.py
@@ -0,0 +1,18 @@
+"""
+你是水果店的老板,店里新推出了DIY水果篮子的服务,可以让用户任意搭配不同品种和数量的水果,
+搭配完后还提供各种不同的打折服务。
+请用用Python实现一个计价程序,方方便收银。提醒:水果的品种、价格和打折的幅度可能随时调整。
+http://codereview.stackexchange.com/questions/20718/the-strategy-design-pattern-for-python-in-a-more-pythonic-way
+http://blog.csdn.net/liuyuan_jq/article/details/6541912
+http://stackoverflow.com/questions/963965/how-is-this-strategy-pattern-written-in-python-the-sample-in-wikipedia
+http://www.laurencegellert.com/2011/08/python-strategy-pattern/
+http://cdn.oreillystatic.com/en/assets/1/event/12/_The%20Lack%20of_%20Design%20Patterns%20in%20Python%20Presentation.pdf
+http://blog.csdn.net/wuyuegb2312/article/details/8962418
+http://www.djangochina.cn/forum.php?mod=viewthread&tid=35
+"""
+
+class Discount(object):
+  def __init__(self):
+    pass
+  
+  def discount()
